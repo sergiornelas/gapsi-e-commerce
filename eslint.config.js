d9fd0348089749834fec123873d6be5b5f9f3f76
sergiornelas <sergio.ornelas.92@outlook.com>
2026-08-23 19:35:36 -0600

    chore/excluir dev-dist generado por vite-plugin-pwa

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,8 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  // `dev-dist` lo genera vite-plugin-pwa durante el desarrollo.
+  globalIgnores(['dist', 'dev-dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
